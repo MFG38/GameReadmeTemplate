8392d69035a3bd3dbf9be0d33693fe0d74a2dec7
MFG38 <santtu.pesonen@mail.com>
2023-12-26 22:06:10 +0200

    create_readme.bat: report error if generation fails

--- create_readme.bat
+++ create_readme.bat
@@ -1,6 +1,8 @@
 @echo off
 
 set /p TARPATH="Enter the file path to your game folder: " % = %
-copy template.txt %TARPATH%\readme.txt
-echo Created readme.txt in %TARPATH%.
+copy template.txt %TARPATH%\readme.txt && (
+    echo Successfully created readme.txt in %TARPATH%. ) || (
+    echo Creation failed.
+)
 pause >nul
